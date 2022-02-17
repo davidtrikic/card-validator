f895878c3839d91adbc76a12df5e744893da8f9b
David <david@Davids-MacBook-Pro.local>
2022-02-17 20:56:32 +0100

    set up tkinter gui

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
-from cgi import test
+# from cgi import test
 from tkinter import *
+# from turtle import width
+
+
+def validate():
+    print(number_entry_value.get())
+    label.config(text=label_value)
+
+def removePlaceholder(*args):
+    number_entry.delete(0, 'end')
 
 # Create window object
 app = Tk()
 
+# Set geometry
+app.resizable(False, False)
+# app.geometry('400x200')
+app.title('Credit Card Validator')
+
+
+# Number entry
+number_entry_value = StringVar()
+number_entry = Entry(app, textvariable=number_entry_value, width=40)
+number_entry.insert(0,'Enter card number...')
+number_entry.bind("<Button-1>", removePlaceholder)
+number_entry.grid(row=0, column=0)
+ 
+#Buttons
+btn_validate = Button(app, text='Validate', width=20, command=validate)
+btn_validate.grid(row=1, column=0)
+
 # Label
-test_text = StringVar()
-test_label = Label(app, text='Name', font=('bold', 14), pady=20)
-test_label.grid(row=0, column=0, sticky=W)
+label = Label(app, text='', font=('bold', 16), pady=20)
+label.grid(row=2, column=0)
+label_value = 'asdfasd'
 
-test_entry = Entry(app, textvariable=test_text)
-test_entry.grid(row=0, column=1)
-
-app.title('Test')
-app.geometry('700x350')
 
 # Start program
 app.mainloop()
